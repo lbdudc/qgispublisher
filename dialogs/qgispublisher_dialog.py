c0b5d64cd9641f976450971c5ca1309cb81e31e7
patrimato <patricia.mato.miragaya@udc.es>
2026-02-09 12:53:23 +0100

    Add checkboxes to layer list

--- dialogs/qgispublisher_dialog.py
+++ dialogs/qgispublisher_dialog.py
@@ -1,6 +1,7 @@
 import os
 from qgis.PyQt import uic
-from qgis.PyQt.QtWidgets import QDialog
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QDialog, QListWidgetItem
 from qgis.core import QgsProject, QgsMapLayer
 from .generate_dialog import GenerateDialog
 from .deploy_dialog import DeployDialog
@@ -22,6 +23,7 @@ class GISPublisherDialog(QDialog, FORM_CLASS):
         self.generateButton.clicked.connect(self.open_generate_dialog)
         self.deployButton.clicked.connect(self.open_deploy_dialog)
         self.cancelButton.clicked.connect(self.close)
+        self.selectAllButton.clicked.connect(self.select_all_layers)
 
         QgsProject.instance().layersAdded.connect(self.load_layers)
         QgsProject.instance().layersRemoved.connect(self.load_layers)
@@ -29,10 +31,20 @@ class GISPublisherDialog(QDialog, FORM_CLASS):
 
     def load_layers(self):
         self.layersList.clear()
-        layers = QgsProject.instance().mapLayers().values()
+
+        project = QgsProject.instance()
+        layers = project.mapLayers().values()
+
         for layer in layers:
-            if layer.type() == QgsMapLayer.VectorLayer:
-                self.layersList.addItem(layer.name())
+            if layer.type() != QgsMapLayer.VectorLayer:
+                continue
+
+            item = QListWidgetItem(layer.name())
+            item.setData(Qt.UserRole, layer.id())
+            item.setCheckState(Qt.Checked)
+
+            self.layersList.addItem(item)
+
         has_layers = self.layersList.count() > 0
         self.generateButton.setEnabled(has_layers)
         self.deployButton.setEnabled(has_layers)
@@ -48,12 +60,38 @@ class GISPublisherDialog(QDialog, FORM_CLASS):
         else:
             req_dialog.exec_()
 
-    def open_generate_dialog(self):
-        layers = [layer for layer in QgsProject.instance().mapLayers().values()
-                if layer.type() == QgsMapLayer.VectorLayer]
-        if not layers:
+    def select_all_layers(self):
+        count = self.layersList.count()
+        if count == 0:
             return
-        self.check_requirements_and_open(lambda: GenerateDialog(layers, parent=self))
+
+        all_checked = True
+        for i in range(count):
+            if self.layersList.item(i).checkState() != Qt.Checked:
+                all_checked = False
+                break
+
+        new_state = Qt.Unchecked if all_checked else Qt.Checked
+
+        for i in range(count):
+            self.layersList.item(i).setCheckState(new_state)
+
+    def open_generate_dialog(self):
+        project = QgsProject.instance()
+        selected_layers = []
+
+        for i in range(self.layersList.count()):
+            item = self.layersList.item(i)
+            if item.checkState() == Qt.Checked:
+                layer_id = item.data(Qt.UserRole)
+                layer = project.mapLayer(layer_id)
+                if layer:
+                    selected_layers.append(layer)
+
+        if not selected_layers:
+            return
+
+        self.check_requirements_and_open(lambda: GenerateDialog(selected_layers, parent=self))
 
     def open_deploy_dialog(self):
         self.check_requirements_and_open(lambda: DeployDialog(parent=self))
